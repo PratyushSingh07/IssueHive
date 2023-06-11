6067e22ab28476ce825f2943eb42ff0fb6bc967b
Pratyush Singh <aries.pratyush@gmail.com>
2023-06-12 00:03:59 +0530

    app: set up adapters & handled click on urls

--- app/src/main/java/com/example/findissues/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/findissues/ui/activity/MainActivity.kt
@@ -3,18 +3,25 @@ package com.example.findissues.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.findissues.R
 import com.example.findissues.models.Issues
 import com.example.findissues.utils.Constants
 import com.example.findissues.api.ServiceHandler
+import com.example.findissues.databinding.ActivityMainBinding
+import com.example.findissues.ui.adapters.IssuesAdapter
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: IssuesAdapter
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         getNews()
     }
 
@@ -25,6 +32,9 @@ class MainActivity : AppCompatActivity() {
                 val news = response.body()
                 if(news!=null){
                     Log.d("RESPONSE",news.toString())
+                    adapter = IssuesAdapter(this@MainActivity, news.items)
+                    binding.issuesList.adapter = adapter
+                    binding.issuesList.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
                 }
             }
 
---
+++ app/src/main/java/com/example/findissues/ui/adapters/IssuesAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.findissues.ui.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.findissues.R
+import com.example.findissues.models.IssuesList
+
+class IssuesAdapter(
+    val context: Context,
+    private val issues: List<IssuesList>,
+): RecyclerView.Adapter<IssuesAdapter.IssuesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IssuesViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_issue, parent, false)
+        return IssuesViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: IssuesViewHolder, position: Int) {
+        val issue = issues[position]
+        holder.link.text = issue.html_url
+        holder.link.setOnClickListener {
+            Toast.makeText(context,"clicked",Toast.LENGTH_SHORT).show()
+            val url = holder.link.text.toString()
+            val browserIntent: Intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            context.startActivity(browserIntent)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return issues.size
+    }
+
+    inner class IssuesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val link = itemView.findViewById<TextView>(R.id.link)
+
+    }
+}
