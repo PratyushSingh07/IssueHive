470daec68a631aff13a623499e0246c84ebd979c
Pratyush Singh <aries.pratyush@gmail.com>
2023-07-04 00:08:52 +0530

    di: @Singleton scope to optimize memory usage

--- app/src/main/java/com/example/findissues/di/NetworkModule.kt
+++ app/src/main/java/com/example/findissues/di/NetworkModule.kt
@@ -2,19 +2,20 @@ package com.example.findissues.di
 
 import com.example.findissues.api.GithubApiService
 import com.example.findissues.utils.Constants
-import com.example.findissues.utils.Constants.PINNED_REPO_BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
 
     @Provides
+    @Singleton
     fun providesRetrofit(): Retrofit {
        return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
@@ -23,6 +24,7 @@ class NetworkModule {
     }
 
     @Provides
+    @Singleton
     fun providesGithubService(retrofit: Retrofit): GithubApiService {
         return retrofit.create(GithubApiService::class.java)
     }
