0fccb344f3ff5513953c22d01cf4134445c9c7fe
Pratyush Singh <aries.pratyush@gmail.com>
2023-08-29 13:03:53 +0530

    refactor: composable to dislpay row items

--- app/src/main/java/com/example/findissues/ui/following/FollowingScreen.kt
+++ app/src/main/java/com/example/findissues/ui/following/FollowingScreen.kt
@@ -1,22 +1,17 @@
 package com.example.findissues.ui.following
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
 import com.example.core.ui.components.AppCard
+import com.example.core.ui.components.AppRowCard
 import com.example.findissues.models.home.Following
 
 @Composable
@@ -31,29 +26,14 @@ fun FollowingScreen(list: List<Following>) {
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
-                Row(
+                AppRowCard(
                     modifier = Modifier
+                        .fillMaxWidth()
                         .background(Color(0xFF0d1117))
-                        .padding(16.dp)
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    AsyncImage(
-                        model = item.avatar_url,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .clip(CircleShape)
-                            .size(64.dp)
-                    )
-                    Spacer(modifier = Modifier.width(24.dp))
-
-                    Text(
-                        text = item.login,
-                        textAlign = TextAlign.Center,
-                        color = Color.White,
-                        fontSize = 16.sp
-                    )
-                }
+                        .padding(16.dp),
+                    imageUrl = item.avatar_url,
+                    text = item.login
+                )
             }
         }
     }
---
+++ core/src/main/java/com/example/core/ui/components/AppRowCard.kt
@@ -0,0 +1,48 @@
+package com.example.core.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+
+@Composable
+fun AppRowCard(
+    modifier: Modifier,
+    verticalAlignment: Alignment.Vertical = Alignment.CenterVertically,
+    imageUrl: String,
+    contentDescription: String? = null,
+    text: String
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = verticalAlignment
+    ) {
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = contentDescription,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(64.dp)
+        )
+
+        Spacer(modifier = Modifier.width(24.dp))
+
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            fontSize = 16.sp
+        )
+    }
+}
